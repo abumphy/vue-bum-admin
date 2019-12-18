39fb37609d4f74487390c551c363fbb64043cdfb
Bumphy <freedom.bumphy258@gmail.com>
2019-12-19 00:22:35 +0800

    登录页

---
+++ src/views/login/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>登录</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
