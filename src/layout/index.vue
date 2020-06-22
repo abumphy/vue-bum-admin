807bbeb4be16d7cf4482fa815cc414f45bd9fde7
Bumphy <freedom.bumphy258@gmail.com>
2020-06-23 00:36:37 +0800

    layout组件完善

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,66 +1,94 @@
 <template>
-  <div class="layout-container">
-    <el-container style="height: 500px; border: 1px solid #eee">
-      <!-- 侧边栏 -->
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1', '3']">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-message"></i>导航一
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-            </el-submenu>
+  <el-container class="layout-container">
+    <!-- 侧边栏 -->
+    <el-aside class="layout-aside" width="200">
+      <div class="logo-container">
+        <img class="logo-img" :src="logo" alt="" />
+      </div>
+      <el-menu
+        class="el-menu-vertical"
+        :default-openeds="['1', '3']"
+        :collapse="isCollapse"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-message"></i>导航一
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
           </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-menu"></i>导航二
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"> <i class="el-icon-menu"></i>导航二 </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
           </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-setting"></i>导航三
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-setting"></i>导航三
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
           </el-submenu>
-        </el-menu>
-      </el-aside>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
 
-      <el-container>
-        
-        <el-header style="text-align: right; font-size: 12px">
+    <!-- 内容区 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header class="layout-header">
+        <div class="header-left">
+          <svg
+  
+      class="hamburger"
+      viewBox="0 0 1024 1024"
+      xmlns="http://www.w3.org/2000/svg"
+      width="64"
+      height="64"
+    >
+      <path d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 0 0 0-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0 0 14.4 7z" />
+    </svg>
+          <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+            <el-radio-button :label="false">展开</el-radio-button>
+            <el-radio-button :label="true">收起</el-radio-button>
+          </el-radio-group>
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+        <div class="header-right">
           <el-dropdown>
             <i class="el-icon-setting" style="margin-right: 15px"></i>
             <el-dropdown-menu slot="dropdown">
@@ -70,26 +98,36 @@
             </el-dropdown-menu>
           </el-dropdown>
           <span>王小虎</span>
-        </el-header>
+        </div>
+      </el-header>
 
-        <el-main>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="日期" width="140"></el-table-column>
-            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-            <el-table-column prop="address" label="地址"></el-table-column>
-          </el-table>
-          <ul id="users">
-      模拟数据
-      <li v-for="user in users" v-bind:key="user.id">{{ user.name }}</li>
-    </ul>
-        </el-main>
-      </el-container>
+      <!-- 主体 -->
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="140"
+          ></el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="120"
+          ></el-table-column>
+          <el-table-column prop="address" label="地址"></el-table-column>
+        </el-table>
+        <ul id="users">
+          模拟数据
+          <li v-for="user in users" v-bind:key="user.id">{{ user.name }}</li>
+        </ul>
+      </el-main>
     </el-container>
-
-  </div>
+  </el-container>
 </template>
 
 <script>
+import logo from "@/assets/logo.png";
+
 export default {
   name: "",
   data() {
@@ -99,8 +137,10 @@ export default {
       address: "上海市普陀区金沙江路 1518 弄"
     };
     return {
-      tableData: Array(20).fill(item),
-      users: []
+      tableData: Array(60).fill(item),
+      users: [],
+      logo: logo,
+      isCollapse: true
     };
   },
 
@@ -108,24 +148,52 @@ export default {
 
   computed: {},
 
-  created(){
+  created() {
     fetch("/api/users")
-        .then((res) => res.json())
-        .then((json) => {
-          this.users = json.users
-        })
+      .then(res => res.json())
+      .then(json => {
+        this.users = json.users;
+      });
   },
 
-  mounted(){},
+  mounted() {},
 
   methods: {}
 };
 </script>
 <style lang='scss' scoped>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
+.layout-container {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+.layout-aside {
+  background-color: rgb(238, 241, 246);
+  .logo-container {
+    display: inline-flex;
+    .logo-img {
+      height: 60px;
+      width: auto;
+    }
+  }
+  .el-menu-vertical:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+}
+
+.layout-header {
+  display: inline-flex;
+  justify-content: space-between;
   line-height: 60px;
+  background-color: #fff;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  .header-left{
+    display: flex;
+    align-items: center;
+  }
 }
 
 .el-aside {
